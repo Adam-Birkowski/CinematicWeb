92d5d4d63aef4eb3fb421d4b4aff31061c249505
Biry <birkowsk@gmail.com>
2020-07-06 22:31:25 +0200

    Second file updated

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -4,6 +4,26 @@ import $ from 'jquery';
 
 
 class PageWrapper extends Component{
+    constructor(){
+        super();
+        this.state = {
+            scrolled: false,
+        };
+    }
+    componentDidMount(){
+        window.addEventListener('scroll', () =>{
+            if ($("#mainNav").offset().top > 100) {
+                $("#mainNav").addClass("navbar-shrink");
+            } else {
+                $("#mainNav").removeClass("navbar-shrink");
+            }
+        })
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll');
+    }
+
 
     clickHandler = (e) => {
         // console.log(e.target.href);
@@ -18,23 +38,24 @@ class PageWrapper extends Component{
         );
 
     }
+
     render(){
         return(
             <div>
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-                <div className="container">
-                    <Link className="navbar-brand js-scroll-trigger" to="/"><img src="assets/img/navbar-logo.svg" onClick={this.clickHandler} data-href="root" alt="" /></Link><button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i className="fas fa-bars ml-1"></i></button>
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
-                        <ul className="navbar-nav text-uppercase ml-auto">
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" onClick={this.clickHandler} data-href="services" to="#services-page-section">Services</Link></li>
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" onClick={this.clickHandler} data-href="portfolio" to="#portfolio">Portfolio</Link></li>
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" onClick={this.clickHandler} data-href="about" to="#about">About</Link></li>
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" onClick={this.clickHandler} data-href="team" to="#team">Team</Link></li>
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger"  onClick={this.clickHandler} data-href="contact" to="#contact">Contact</Link></li>
-                        </ul>
-                    </div>
+            <nav className={this.state.scrolled ? 'navbar-shrink': 'navbar navbar-expand-lg navbar-dark fixed-top'} id="mainNav">
+                    <div className="container">
+                        <Link className="navbar-brand js-scroll-trigger" to="/"><img src="assets/img/navbar-logo.svg" onClick={this.clickHandler} data-href="root" alt="" /></Link><button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i className="fas fa-bars ml-1"></i></button>
+                        <div className="collapse navbar-collapse" id="navbarResponsive">
+                            <ul className="navbar-nav text-uppercase ml-auto">
+                                <li className="nav-item"><Link className="nav-link js-scroll-trigger" onClick={this.clickHandler} data-href="services" to="#services-page-section">Services</Link></li>
+                                <li className="nav-item"><Link className="nav-link js-scroll-trigger" onClick={this.clickHandler} data-href="portfolio" to="#portfolio">Portfolio</Link></li>
+                                <li className="nav-item"><Link className="nav-link js-scroll-trigger" onClick={this.clickHandler} data-href="about" to="#about">About</Link></li>
+                                <li className="nav-item"><Link className="nav-link js-scroll-trigger" onClick={this.clickHandler} data-href="team" to="#team">Team</Link></li>
+                                <li className="nav-item"><Link className="nav-link js-scroll-trigger"  onClick={this.clickHandler} data-href="contact" to="#contact">Contact</Link></li>
+                            </ul>
+                        </div>
 
-                </div>
+                    </div>
             </nav>
             {this.props.children}
             </div>
